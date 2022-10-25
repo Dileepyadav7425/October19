127cfa6d2f62d6d590deaaafe137831c32a1bf9a
Dileepyadav7425 <dileepyadav7425@gmail.com>
2022-10-25 12:39:04 +0530

    assignments

---
+++ hooks-concept/src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Form from "./Form";
+import Table from "./Table";
+ 
+ const App=()=>{
+
+ return (
+  // <Form/>
+  <Table/>
+ )
+
+ }
+ export default App;
---
+++ hooks-concept/src/Form.js
@@ -0,0 +1,14 @@
+import App from "./App";
+const Form=()=>{
+
+    return(<>
+        
+   Name <input type="text" />
+    <br/>
+   Number<input type="number"/>
+   <br/>
+    <button>Submit</button>
+
+        </>)
+}
+export default Form;
