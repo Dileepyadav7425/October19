238cf80f56a1178133774c421940fd689de7a472
Dileepyadav7425 <dileepyadav7425@gmail.com>
2022-11-07 21:58:24 +0530

    code

--- reactmock/todo-list/src/App.js
+++ reactmock/todo-list/src/App.js
@@ -1,13 +1,29 @@
-import Header from "./Header";
- import'./App.css'
-
+import React,{useState} from "react"
 
 function App(){
+  const[activity,setActivity]=useState('')
+  const[listData,setlistData]=useState([])
 
-return (<div className="App">
- 
-  <Header/>
-</div>)
-
+  function addActivity(i){
+    setActivity('')
+    setlistData([...listData,activity])
+  }
+  function removeActivity(i){
+    const updatedList=listData.filter((elem)=>{
+      return i!==elem;
+    })
+      setlistData(updatedList)
+  }
+  return(
+    <>
+    <input type="text" placeholder="Enterlist" value={activity} onChange={(e)=>setActivity
+      (e.target.value)}/>
+     <button onClick={addActivity}>Add</button>
+     <div>
+      {listData.map((i,index)=><ol>{index+1} {i} 
+      <button onClick={()=>removeActivity(i)}>Remove</button></ol>)}
+     </div>
+    </>
+  )
 }
 export default App;
